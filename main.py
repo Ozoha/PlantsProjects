91848f6f7d0f00fd78b37310d93661a3c478ad7b
Oz Ohayon <Oz.ouchyon@gmail.com>
2022-02-23 10:17:01 +0200

    Created docker-compose.yml for Kibana/ElasticSearch/Logstash.
    Added as docker:
    - BCL2fastq
    - fastqc
    - multiqc
    - cutadapt
    - dada2

--- main.py
+++ main.py
@@ -1,21 +1,49 @@
 
-from ElasticSearch.elastic_search_integraion_package import ESIndexManager
+from ElasticSearch.elastic_search_integraion_package import ESIndexManager, SampleDoc, TaxonomyDoc
+from ElasticSearch.read_excel_meta_data import ReadExcel
 
 if __name__ == '__main__':
-    file = r'C:\Users\Oz Ohayon\user data\meta_data.xlsx'
-    mapping = {
-        "mappings": {
-            "properties": {
-                "location": {
-                    "type": "geo_point"
-                }
+    test_sample = True
+    test_taxonomy = True
+    file_sample = r'C:\Users\Oz Ohayon\user data\meta_data.xlsx'
+    file_taxonomy = r"C:\Users\Oz Ohayon\user data\taxonomy_levels_sample_28.xlsx"
+
+    mapping_sample = {
+        "properties": {
+            "location": {
+                "type": "geo_point"
             }
         }
     }
 
-    sample_manager = ESIndexManager('sample', file)
+    mapping_taxonomy = {
+        "mappings": {
+            "properties": {
+            }
+         }
+    }
 
-    if not sample_manager.create_index(mapping):
-        exit(1)
 
-    failed_docs = sample_manager.add_documents()
+    # Creating Docs:
+    index_sample = 'test_plant'
+    index_taxonomy = 'test_taxonomy2'
+
+    sample_reader = ReadExcel(file_sample, index_col=0)
+    taxonomy_reader = ReadExcel(file_taxonomy)
+
+    sample_docs = sample_reader.read()
+    taxonomy_docs = taxonomy_reader.read()
+    # taxonomy_docs = taxonomy_reader.get_groups()
+    index_manager = ESIndexManager()
+
+    if test_sample:
+        if not index_manager.create_index(index_sample, mapping_sample):
+            exit(1)
+        failed_docs = index_manager.add_documents(index_sample, SampleDoc, sample_docs)
+    if test_taxonomy:
+        # if not index_manager.create_index(index_taxonomy):
+        #     exit(1)
+        failed_docs = index_manager.add_documents(index_taxonomy, TaxonomyDoc, taxonomy_docs)
+
+
+
--- react-nodejs-docker-compose/my-app/src/components/Header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Header = () => {
-    return(
-        <div className="header">
-            <h1>Add Sample</h1>
-        </div>
-    )
-}
